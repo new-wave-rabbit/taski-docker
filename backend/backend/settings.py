07e62b3055718707c9c538fd9939ff4f679df374
idotsenko <idotsenko@usergate.com>
2026-03-15 14:11:24 +0300

    добавлено доменное имя

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,10 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '123.123.123.123'] 
-
-
-# Application definition
+ALLOWED_HOSTS = ['localhost', '123.123.123.123', 'yp-manual-deploy.dis-lcl.ru']
 
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
