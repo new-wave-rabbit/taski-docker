e5253bbbdfc9d1de0e1cde8636769c77c1c9e7ca
idotsenko <idotsenko@usergate.com>
2026-03-15 14:05:50 +0300

    awd

--- first_run.sh
+++ first_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker_compose_file=docker-compose.production.yml
 
-docker compose -f $docker_compose_file up
+docker compose -f $docker_compose_file up -d
 docker compose -f $docker_compose_file exec backend python manage.py collectstatic
 docker compose -f $docker_compose_file exec backend cp -r /app/collected_static/. /backend_static/static/
